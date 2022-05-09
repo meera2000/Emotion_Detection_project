d7a3b9f052f1f714c654f7ce2e8c9e992d28152b
meera2000 <65386360+meera2000@users.noreply.github.com>
2022-05-09 18:52:57 +0530

    Add files via upload

---
+++ model_file.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense,Input,Dropout,GlobalAveragePooling2D,Flatten,Conv2D,BatchNormalization,Activation,MaxPooling2D
+from keras.models import Model,Sequential
+from tensorflow.keras.optimizers import Adam,SGD,RMSprop
+
+
+# In[5]:
+
+
+picture_size = 48
+folder_path = "E:/sc project/images/"
+
+
+# In[6]:
+
+
+batch_size  = 128
+
+datagen_train  = ImageDataGenerator()
+datagen_val = ImageDataGenerator()
+
+train_set = datagen_train.flow_from_directory(folder_path+"train",
+                                              target_size = (picture_size,picture_size),
+                                              color_mode = "grayscale",
+                                              batch_size=batch_size,
+                                              class_mode='categorical',
+                                              shuffle=True)
+
+
+test_set = datagen_val.flow_from_directory(folder_path+"validation",
+                                              target_size = (picture_size,picture_size),
+                                              color_mode = "grayscale",
+                                              batch_size=batch_size,
+                                              class_mode='categorical',
+                                              shuffle=False)
+
+
+# In[7]:
+
+
+no_of_classes = 5
+
+model = Sequential()
+
+#1st CNN layer
+model.add(Conv2D(64,(3,3),padding = 'same',input_shape = (48,48,1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout(0.25))
+
+#2nd CNN layer
+model.add(Conv2D(128,(5,5),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout (0.25))
+
+#3rd CNN layer
+model.add(Conv2D(512,(3,3),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout (0.25))
+
+#4th CNN layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+#Fully connected 1st layer
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+
+# Fully connected layer 2nd layer
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(no_of_classes, activation='softmax'))
+
+
+
+opt = Adam(lr = 0.0001)
+model.compile(optimizer=opt,loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+
+# In[ ]:
+
+
+epochs = 48
+history= model.fit_generator(generator=train_set,
+                                steps_per_epoch=train_set.n//train_set.batch_size,
+                                epochs=epochs,
+                                validation_data = test_set,
+                                validation_steps = test_set.n//test_set.batch_size,
+
+                                )
+
+
+# In[ ]:
+
+
+model.save('./model_last1 .h5')
+
